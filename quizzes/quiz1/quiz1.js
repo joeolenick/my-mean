37c7aea76500b6f9974c3229aedbf7fb036e4a6b
joe Olenick <jolenick1298@gmail.com>
2015-02-12 21:08:27 -0500

    first quiz

---
+++ quizzes/quiz1/quiz1.js
@@ -0,0 +1,11 @@
+//Arrays
+//-------------------------
+//
+//Create an array of 5 items (numbers or strings) and assign in to a variable.
+//Write a FOR loop to iterate over the array and print out each item to the console.
+
+var array = [1,2,3,4,5]
+
+for (var i = 0; i < array.length; i++){
+   console.log(i)
+}
