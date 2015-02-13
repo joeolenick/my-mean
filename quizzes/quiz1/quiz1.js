92bda7bfbdcdd3c5a289fc3872aa1617beb5b179
joe Olenick <jolenick1298@gmail.com>
2015-02-12 21:29:39 -0500

    I was wrong, this is right.

--- quizzes/quiz1/quiz1.js
+++ quizzes/quiz1/quiz1.js
@@ -4,8 +4,8 @@
 //Create an array of 5 items (numbers or strings) and assign in to a variable.
 //Write a FOR loop to iterate over the array and print out each item to the console.
 
-var array = [1,2,3,4,5]
+var array = ["fish","taco","hot dogs","pizza","soup"]
 
 for (var i = 0; i < array.length; i++){
-   console.log(i)
+   console.log(array[i])
 }
