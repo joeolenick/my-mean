778f57376baa37ded56a454ec1f267f9d4b463f2
joe Olenick <jolenick1298@gmail.com>
2015-02-18 21:42:52 -0500

    quiz3 add

---
+++ quizzes/quiz3/quiz3.js
@@ -0,0 +1,20 @@
+var strngFin = '';
+var noVowelsArray = '';
+
+var noVowels = function(string){
+	for (var i = 0; i < string.length ; i++) {
+		if(	string[i].toLowerCase() != 'a' &&
+			string[i].toLowerCase() != 'e' &&
+			string[i].toLowerCase() != 'i' &&
+			string[i].toLowerCase() != 'o' &&
+			string[i].toLowerCase() != 'u' &&
+			string[i].toLowerCase() != 'y'	)
+			strngFin += string[i];
+	}
+var noVowelsArray = strngFin.toString(',');
+return noVowelsArray;
+}
+
+
+console.log(noVowels("The quick brown fox jumps over the lazy dog"));
+
